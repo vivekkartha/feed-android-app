88a153d71c0b3259016118ffa4c296e078cd4a95
Vivek Kartha <vivek.kartha@gmail.com>
2018-09-26 03:27:14 +0530

    Added FeedRecyclerAdapter and set list data

---
+++ app/src/main/java/com/feed/app/ui/home/FeedRecyclerAdapter.kt
@@ -0,0 +1,49 @@
+package com.feed.app.ui.home
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.feed.app.R
+import com.feed.app.data.FeedItem
+import com.squareup.picasso.Picasso
+
+class FeedRecyclerAdapter(
+  private var feedList: List<FeedItem>,
+  private var ctx: Context
+) : RecyclerView.Adapter<FeedRecyclerAdapter.ViewHolder>() {
+
+  override fun onCreateViewHolder(
+    parent: ViewGroup,
+    viewType: Int
+  ): ViewHolder {
+    val view = LayoutInflater.from(parent.context)
+        .inflate(R.layout.item_feed, parent, false)
+    return ViewHolder(view)
+  }
+
+  override fun getItemCount() = feedList.size
+
+  override fun onBindViewHolder(
+    holder: ViewHolder,
+    position: Int
+  ) {
+    val feed = feedList[position]
+    holder.apply {
+      Picasso.get()
+          .load(feed.imageHref)
+          .into(ivFeed)
+      tvTitle.text = feed.title
+      tvDescription.text = feed.description
+    }
+  }
+
+  inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+    val ivFeed: ImageView = v.findViewById(R.id.ivFeed)
+    val tvTitle: TextView = v.findViewById(R.id.tvTitle)
+    val tvDescription: TextView = v.findViewById(R.id.tvDescription)
+  }
+}
--- app/src/main/java/com/feed/app/ui/home/HomeActivity.kt
+++ app/src/main/java/com/feed/app/ui/home/HomeActivity.kt
@@ -4,16 +4,41 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.feed.app.R.layout
+import com.feed.app.R
+import com.feed.app.data.FeedItem
+import com.feed.app.utils.di.FeedComponent
+import com.feed.app.utils.di.FeedModule
+import kotlinx.android.synthetic.main.home_activity.rvFeed
 
 class HomeActivity : AppCompatActivity() {
 
+  private lateinit var feedRecyclerAdapter: FeedRecyclerAdapter
+  private var feedList = mutableListOf<FeedItem>()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(layout.activity_main)
-    val viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-    viewModel.feedLiveData.observe(this, Observer {
-      //TODO: Set to list
+    setContentView(R.layout.home_activity)
+    val viewModel = ViewModelProviders.of(this)
+        .get(HomeViewModel::class.java)
+    injectDependencies()
+    initList()
+    setFeedsToList(viewModel)
+  }
+
+  private fun injectDependencies() {
+    FeedComponent.instance = FeedModule()
+  }
+
+  private fun initList() {
+    feedRecyclerAdapter = FeedRecyclerAdapter(feedList, this)
+    rvFeed.adapter = feedRecyclerAdapter
+  }
+
+  private fun setFeedsToList(viewModel: HomeViewModel) {
+    viewModel.feedLiveData.observe(this, Observer { feed ->
+      feedList.clear()
+      feedList.addAll(feed?.rows!!)
+      feedRecyclerAdapter.notifyDataSetChanged()
     })
   }
 }
